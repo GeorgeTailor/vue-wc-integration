0778732ede4b6229ffeca718b58c4c1ec844dc93
Yuriy Kravets <yucikkravets@gmail.com>
2020-12-05 13:46:43 +0100

    update repo

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,21 @@
 <template>
   <div id="app">
-    <zoo-header imgsrc="assets/logo.png" headertext="Zoo web components in Vue"></zoo-header>
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <zoo-header headertext="Zoo web components in Vue">
+      <img slot="img" alt="Zooplus logo" src="assets/logo.png"/>
+    </zoo-header>
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from "vue-property-decorator";
-import HelloWorld from "./components/HelloWorld.vue";
-import "@zooplus/zoo-web-components";
+import { Component, Vue } from 'vue-property-decorator';
+import HelloWorld from './components/HelloWorld.vue';
+import '@zooplus/zoo-web-components';
 
 @Component({
   components: {
-    HelloWorld
-  }
+    HelloWorld,
+  },
 })
 export default class App extends Vue {}
 </script>
@@ -25,5 +27,18 @@ export default class App extends Vue {}
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+}
+:root {
+  --primary-mid: #3c9700;
+  --primary-light: #66b100;
+  --primary-dark: #286400;
+  --primary-ultralight: #ebf4e5;
+  --secondary-mid: #ff6200;
+  --secondary-light: #f80;
+  --secondary-dark: #cc4e00;
+  --info-ultralight: #ecf5fa;
+  --info-mid: #459fd0;
+  --warning-ultralight: #fde8e9;
+  --warning-mid: #ed1c24;
 }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,56 +1,68 @@
 <template>
-	<form class="form">
-		<zoo-select inputerrormsg="Name is required" :invalid="!nameValid">
-			<select id="select-id1" formControlName="select" slot="selectelement">
-				<option v-for="option in options" :value="option.id">{{ option.firstName }} {{ option.lastName }}</option>
-			</select>
-			<label for="select-id1" slot="selectlabel">Name</label>
-		</zoo-select>
-		<zoo-input labeltext="Input date field" inputerrormsg="Invalid value" infotext="Information text" :invalid="!dateValid">
-			<input id="input-id1" type="date" placeholder="Placeholder" slot="inputelement"/>
-			<label for="input-id1" slot="inputlabel">Input date field</label>
-		</zoo-input>
-		<zoo-button class="submit-button" size="small" v-on:click="submit()">
-			<button type="button">Submit</button>
-		</zoo-button>
-	</form>
+  <form class="form">
+    <zoo-select inputerrormsg="Name is required" :invalid="!nameValid">
+      <select id="select-id1" formControlName="select" slot="selectelement">
+        <option v-for="option in options" v-bind:key="option.id" :value="option.id">
+          {{ option.firstName }} {{ option.lastName }}
+        </option>
+      </select>
+      <label for="select-id1" slot="selectlabel">Name</label>
+    </zoo-select>
+    <zoo-input
+      labeltext="Input date field"
+      inputerrormsg="Invalid value"
+      infotext="Information text"
+      :invalid="!dateValid"
+    >
+      <input
+        id="input-id1"
+        type="date"
+        placeholder="Placeholder"
+        slot="inputelement"
+      />
+      <label for="input-id1" slot="inputlabel">Input date field</label>
+    </zoo-input>
+    <zoo-button v-on:click="submit()">
+      <button type="button">Submit</button>
+    </zoo-button>
+  </form>
 </template>
 
 <script lang="ts">
-	import { Component, Prop, Vue } from 'vue-property-decorator';
+import { Component, Prop, Vue } from 'vue-property-decorator';
 
-	@Component
-	export default class HelloWorld extends Vue {
-		private options = [
-			{
-				firstName: 'firstName',
-				lastName: 'lastName'
-			}
-		];
-		private name: any;
-		private nameValid = true;
-		private date: any;
-		private dateValid = true;
+@Component
+export default class HelloWorld extends Vue {
+  private options = [
+    {
+      firstName: 'firstName',
+      lastName: 'lastName',
+    },
+  ];
+  private name: any;
+  private nameValid = true;
+  private date: any;
+  private dateValid = true;
 
-		submit() {
-			if (this.name && this.date) {
-				return true;
-			}
+  public submit() {
+    if (this.name && this.date) {
+      return true;
+    }
 
-			if (!this.name) {
-				this.nameValid = false;
-			}
-			if (!this.date) {
-				this.dateValid = false;
-			}
-		}
-	}
+    if (!this.name) {
+      this.nameValid = false;
+    }
+    if (!this.date) {
+      this.dateValid = false;
+    }
+  }
+}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="css">
-	.form {
-		max-width: 768px;
-		margin: 0 auto;
-	}
+.form {
+  max-width: 768px;
+  margin: 0 auto;
+}
 </style>
